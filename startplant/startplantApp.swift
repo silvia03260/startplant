a0085df316ff777cc6bacad2b8195dc8ae4859e6
silvia03260 <silvia.lembo32@gmail.com>
2024-12-10 15:51:09 +0100

    Initial Commit

---
+++ startplant/startplantApp.swift
@@ -0,0 +1,17 @@
+//
+//  startplantApp.swift
+//  startplant
+//
+//  Created by Silvia Lembo on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct startplantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
